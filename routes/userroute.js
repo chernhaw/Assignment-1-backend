f30e647ce25d4aff81d947e859ad91923d63e28e
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-07-06 14:14:46 +0800

    Add files via upload

---
+++ routes/userroute.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+const userroute  = express.Router(); 
+const usermgt = require('../controller/usermgt')
+
+userroute.post('/login', usermgt.login)
+userroute.post('/email', usermgt.email)
+userroute.post('/admin', usermgt.admin)
+userroute.post('/updateemail',usermgt.updateemail )
+userroute.post('/updatepass',usermgt.updatepass )
+userroute.post('/activate', usermgt.activate)
+userroute.post('/newuser', usermgt.newuser)
+userroute.post('/checkgroup', usermgt.checkgroup)
+userroute.post('/byemail', usermgt.byemail)
+userroute.post('/byusername', usermgt.byusername)
+userroute.post('/updateadm', usermgt.updateadm)
+userroute.post('/checkusergroup', usermgt.checkusergroup)
+userroute.post('/creategroup', usermgt.creategroup)
+userroute.post('/groupassign', usermgt.groupassign)
+userroute.post('/userexist', usermgt.userexist)
+userroute.post('/groupremove', usermgt.groupremove)
+module.exports = userroute;
