1badd3a7702849f08d76cf75e7584233a31f9153
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-07-09 01:21:08 +0800

    Add files via upload

--- routes/userroute.js
+++ routes/userroute.js
@@ -5,22 +5,28 @@ const usermgt = require('../controller/usermgt')
 
 userroute.post('/login', usermgt.login)
 userroute.post('/email', usermgt.email)
-userroute.post('/admin', usermgt.admin)
+userroute.post('/admin', usermgt.checkgroup)
 userroute.post('/updateemail',usermgt.updateemail )
 userroute.post('/updatepass',usermgt.updatepass )
 userroute.post('/activate', usermgt.activate)
 userroute.post('/newuser', usermgt.newuser)
+
+
 userroute.post('/checkgroup', usermgt.checkgroup)
+
+
 userroute.post('/byemail', usermgt.byemail)
 userroute.post('/byusername', usermgt.byusername)
 userroute.post('/updateadm', usermgt.updateadm)
-userroute.post('/checkusergroup', usermgt.checkusergroup)
+
 userroute.post('/creategroup', usermgt.creategroup)
 userroute.post('/groupassign', usermgt.groupassign)
 userroute.post('/userexist', usermgt.userexist)
 userroute.post('/groupremove', usermgt.groupremove)
 
-userroute.post('/grouprole', usermgt.checkusergroup)
+userroute.post('/listgroup', usermgt.listgroup)
+userroute.post('/listusers', usermgt.listusers)
+userroute.post('/checkgroup', usermgt.checkgroup)
 userroute.post('/groupadminassign', usermgt.groupadminassign)
 userroute.post('/groupadminremove', usermgt.groupadminremove)
 module.exports = userroute;
