b42f3498fce1c03abaf74a9c8cdc555ac9e782a7
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-08-01 17:38:03 +0800

    Add files via upload

--- routes/approute.js
+++ routes/approute.js
@@ -4,9 +4,9 @@ const approute  = express.Router();
 const appmgt = require('../controller/appmgt')
 
 approute.post('/createapp', appmgt.createapp)
-
-
-
+approute.post('/checkapp', appmgt.checkapp)
+approute.post('/listapp', appmgt.listapp)
+approute.post('/updateapp', appmgt.updateapp)
 
 //listlistenabledusers
 //listdisabledusers
