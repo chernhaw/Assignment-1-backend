735f194759c6732b2e3c15175f00000c1bb079f1
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-08-02 17:54:04 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -5,7 +5,8 @@
  var bodyParser = require('body-parser')
  const userroute = require('./routes/userroute')
  const approute = require('./routes/approute')
- const planroute = require('./routes/planroute')
+ const planroute = require('./routes/planroute');
+const taskroute = require('./routes/taskroute');
 // // TODO : split into route and put env into a file import env file
   var port = process.env.PORT || 8080;
  
@@ -16,7 +17,7 @@
   // added as per https://stackoverflow.com/questions/37222313/request-body-is-null-for-post-request-in-nodejs
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.use('/', userroute, approute, planroute)
+  app.use('/', userroute, approute, planroute, taskroute)
 
 app.get('/api',function(req, res){
     res.json({firstname:'John', lastname:'Doe'});
