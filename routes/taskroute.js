b6d37768bad0f5e76952630d46cc56e4d308667f
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-08-05 15:36:34 +0800

    Add files via upload

--- routes/taskroute.js
+++ routes/taskroute.js
@@ -6,5 +6,7 @@ const taskmgt = require('../controller/taskmgt')
 taskroute.post('/createtask', taskmgt.createtask)
 taskroute.post('/counttask', taskmgt.counttask)
 taskroute.post('/listapptasks', taskmgt.getAllTasksByApp)
-
+taskroute.post('/gettaskdetail', taskmgt.getTaskDetail)
+taskroute.post('/getaccess', taskmgt.groupaccess)
+taskroute.post('/updatetask', taskmgt.updateTask)
 module.exports = taskroute;
